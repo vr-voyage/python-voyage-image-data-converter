3d87c0b0b9035c0e864027410f996d94140ab6ee
Voyage <voyage@miouyouyou.fr>
2026-01-29 18:40:25 +0100

    Dealing with non multiple of 4
    Basically, replicating the code from the main
    converter...
    
    I'll have to find a way to combine both projects
    into one.
    Meanwhile, this is done and tested !
    No more "mulitple of 4" errors !
    
    Signed-off-by: Voyage <voyage@miouyouyou.fr>

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,50 @@
 use pyo3::prelude::*;
 
 use ddsfile::{AlphaMode, Caps2, D3D10ResourceDimension, Dds, DxgiFormat, NewDxgiParams};
-use image::ImageReader;
-use std::io::Cursor;
+use intel_tex_2::bc5;
 use intel_tex_2::bc7;
-use intel_tex_2::bc3;
+use std::fmt;
+use std::io::Cursor;
 
-fn convert_to_bc7(buffer: &[u8]) -> (u32, u32, Vec<u8>)
+#[pyclass]
+pub enum CompressionFormat
 {
-    let img = ImageReader::new(Cursor::new(buffer)).with_guessed_format().unwrap().decode().unwrap();
-    let rgba8_image = img.to_rgba8();
-    let width = rgba8_image.width();
-    let height = rgba8_image.height();
+    Rgba8,
+    Rgba8Unorm,
+    Dxt5,
+    Bc7
+}
 
-    let rgba8_content = &rgba8_image.into_raw()[..];
-    /*for b in 0..rgba8_content.len()
-    {
-        if (b & 7) == 0 { print!("\n"); }
-        print!("0x{:x} ", rgba8_content[b]);
+impl fmt::Display for CompressionFormat {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            CompressionFormat::Rgba8 => write!(f, "RGBA8"),
+            CompressionFormat::Dxt5 => write!(f, "DXT5"),
+            CompressionFormat::Bc7 => write!(f, "BC7"),
+            CompressionFormat::Rgba8Unorm => write!(f, "RGBA8_UNORM")
+        }
     }
-    print!("\n");*/
+}
 
+fn compresion_format_to_dxgi_format(compression_format:&CompressionFormat) -> DxgiFormat
+{
+    return match compression_format {
+        CompressionFormat::Bc7 => DxgiFormat::BC7_UNorm_sRGB,
+        CompressionFormat::Rgba8 => DxgiFormat::R8G8B8A8_UInt,
+        CompressionFormat::Rgba8Unorm => DxgiFormat::R8G8B8A8_UNorm,
+        CompressionFormat::Dxt5 => DxgiFormat::BC3_UNorm_sRGB,
+    }
+}
+
+fn convert_image_as_raw_rgba8(width: u32, height: u32, rgba8_content: &[u8]) -> (u32, u32, Vec<u8>, CompressionFormat)
+{
+    return (width, height, rgba8_content.to_vec(), CompressionFormat::Rgba8);
+}
+
+type SurfaceHandler = fn(&intel_tex_2::RgbaSurface, &mut [u8]);
+
+fn convert_image_as(width: u32, height: u32, rgba8_content: &[u8], format: DxgiFormat, surface_handler: SurfaceHandler) -> (u32, u32, Vec<u8>, CompressionFormat)
+{
     let block_count = intel_tex_2::divide_up_by_multiple(width * height, 16);
     println!("Block count: {}", block_count);
     println!("width {} - height {}", width, height);
@@ -28,7 +52,7 @@ fn convert_to_bc7(buffer: &[u8]) -> (u32, u32, Vec<u8>)
         height,
         width,
         depth: Some(1),
-        format: DxgiFormat::BC7_UNorm,
+        format: format, //  DxgiFormat::BC7_UNorm,
         mipmap_levels: Some(1),
         array_layers: Some(1),
         caps2: Some(Caps2::empty()),
@@ -37,91 +61,74 @@ fn convert_to_bc7(buffer: &[u8]) -> (u32, u32, Vec<u8>)
         alpha_mode: AlphaMode::Straight,
     };
     // BC7
-    {
-        let mut dds = Dds::new_dxgi(NewDxgiParams {
-            format: DxgiFormat::BC7_UNorm,
-            ..dds_defaults
-        })
-        .unwrap();
-        let surface = intel_tex_2::RgbaSurface {
-            width,
-            height,
-            stride: width * 4,
-            data: rgba8_content,
-        };
-
-        println!("Compressing to BC7...");
-        bc7::compress_blocks_into(
-            &bc7::alpha_ultra_fast_settings(),
-            &surface,
-            dds.get_mut_data(0 /* layer */).unwrap(),
-        );
-        println!("  Done!");
-
-        //dds.write(&mut OpenOptions::new().write(true).create(true).open("a.dds").unwrap());
-        let dds_data = dds.get_data(0).unwrap();
-        return (width, height, dds_data.to_vec());
-    }
-}
-
-fn convert_to_dxt5(buffer: &[u8]) -> (u32, u32, Vec<u8>)
-{
-    let img = ImageReader::new(Cursor::new(buffer)).with_guessed_format().unwrap().decode().unwrap();
-    let rgba8_image = img.to_rgba8();
-    let width = rgba8_image.width();
-    let height = rgba8_image.height();
-
-    let rgba8_content = &rgba8_image.into_raw()[..];
-    /*for b in 0..rgba8_content.len()
-    {
-        if (b & 7) == 0 { print!("\n"); }
-        print!("0x{:x} ", rgba8_content[b]);
-    }
-    print!("\n");*/
-
-    let block_count = intel_tex_2::divide_up_by_multiple(width * height, 16);
-    println!("Block count: {}", block_count);
-    println!("width {} - height {}", width, height);
-    let dds_defaults = NewDxgiParams {
-        height,
+    let mut dds = Dds::new_dxgi(NewDxgiParams {
+        format: format,  // DxgiFormat::BC7_UNorm,
+        ..dds_defaults
+    })
+    .unwrap();
+    let surface = intel_tex_2::RgbaSurface {
         width,
-        depth: Some(1),
-        format: DxgiFormat::BC3_Typeless,
-        mipmap_levels: Some(1),
-        array_layers: Some(1),
-        caps2: Some(Caps2::empty()),
-        is_cubemap: false,
-        resource_dimension: D3D10ResourceDimension::Texture2D,
-        alpha_mode: AlphaMode::Straight,
+        height,
+        stride: width * 4,
+        data: rgba8_content,
     };
-    // BC7
-    {
-        let mut dds = Dds::new_dxgi(NewDxgiParams {
-            format: DxgiFormat::BC3_Typeless,
-            ..dds_defaults
-        })
-        .unwrap();
-        let surface = intel_tex_2::RgbaSurface {
-            width,
-            height,
-            stride: width * 4,
-            data: rgba8_content,
-        };
+    surface_handler(&surface, dds.get_mut_data(0 /* layer */).unwrap());
+    println!("Compressing to BC7...");
+    
+    println!("  Done!");
 
-        println!("Compressing to DXT5...");
-        bc3::compress_blocks_into(
-            &surface,
-            dds.get_mut_data(0 /* layer */).unwrap(),
-        );
-        println!("  Done!");
+    //dds.write(&mut OpenOptions::new().write(true).create(true).open("a.dds").unwrap());
+    let dds_data = dds.get_data(0).unwrap();
+    return (width, height, dds_data.to_vec(), CompressionFormat::Bc7);
+}
 
-        //dds.write(&mut OpenOptions::new().write(true).create(true).open("a.dds").unwrap());
-        let dds_data = dds.get_data(0).unwrap();
-        return (width, height, dds_data.to_vec());
+fn align_on(pow2_value: u32, val: u32) -> u32
+{
+    let mask: u32 = pow2_value - 1;
+    return (val+mask) & (!mask);
+}
+
+fn surface_treatment_none(_surface: &intel_tex_2::RgbaSurface, _blocks: &mut [u8])
+{
+
+}
+
+fn surface_treatment_dxt5(surface: &intel_tex_2::RgbaSurface, blocks: &mut [u8])
+{
+    bc5::compress_blocks_into(surface, blocks);
+}
+
+fn surface_treatment_bc7(surface: &intel_tex_2::RgbaSurface, blocks: &mut [u8])
+{
+    bc7::compress_blocks_into(
+        &bc7::alpha_ultra_fast_settings(),
+        &surface,
+        blocks // dds.get_mut_data(0 /* layer */).unwrap(),
+    );
+}
+
+fn surface_handler_for(compression_format:&CompressionFormat) -> SurfaceHandler
+{
+    return match compression_format {
+        CompressionFormat::Bc7 => surface_treatment_bc7,
+        CompressionFormat::Rgba8 => surface_treatment_none,
+        CompressionFormat::Rgba8Unorm => surface_treatment_none,
+        CompressionFormat::Dxt5 => surface_treatment_dxt5,
     }
 }
 
-/// Formats the sum of two numbers as string.
+fn compression_format_from_name(name:&str) -> CompressionFormat
+{
+    
+    return match name.to_lowercase().as_str().into() {
+        "bc7" => CompressionFormat::Bc7,
+        "rgba8" => CompressionFormat::Rgba8,
+        "rgba8_unorm" => CompressionFormat::Rgba8Unorm,
+        "dxt5" => CompressionFormat::Dxt5,
+        _ => CompressionFormat::Bc7
+    };
+}
+
 /**
  * Convert the image provided to DDS, compressed using the BC7 algorithm
  * @param buffer The buffer containing the image data to convert
@@ -129,10 +136,36 @@ fn convert_to_dxt5(buffer: &[u8]) -> (u32, u32, Vec<u8>)
  * @note The returned buffer has no header
  */
 #[pyfunction]
-fn convert_image_content_in(buffer: &[u8], dxt: bool) -> PyResult<(u32, u32, Vec<u8>)>
+pub fn convert_image_content_in(buffer: &[u8], preferred_compression_format:&str) -> PyResult<(u32, u32, Vec<u8>, CompressionFormat)>
 {
-    if dxt { return Ok(convert_to_dxt5(buffer)); }
-    return Ok(convert_to_bc7(buffer));
+    let img = image::ImageReader::new(Cursor::new(buffer)).with_guessed_format().unwrap().decode().unwrap();
+    
+    let width = img.width();
+    let height = img.height();
+
+    if (width * height) < (256*256)
+    {
+        let rgba8_image = img.to_rgba8();
+        let rgba8_content = &rgba8_image.into_raw()[..];
+        return Ok(convert_image_as_raw_rgba8(width, height, rgba8_content));
+    }
+    else
+    {
+        let compression_format = compression_format_from_name(preferred_compression_format);
+        let dxgi_format = compresion_format_to_dxgi_format(&compression_format);
+        let surface_handler = surface_handler_for(&compression_format);
+
+        let used_width = align_on(4, width);
+        let used_height  = align_on(4, height);
+        let resize_filter:image::imageops::FilterType = image::imageops::FilterType::Lanczos3;
+
+        let needs_resize :bool = (used_width != width) | (used_height != height);
+        let used_img: image::DynamicImage = if needs_resize { img.resize_exact(used_width, used_height, resize_filter) } else { img };
+
+        let rgba8_image = used_img.to_rgba8();
+        let rgba8_content = &rgba8_image.into_raw()[..];
+        return Ok(convert_image_as(used_width, used_height, rgba8_content, dxgi_format, surface_handler));
+    }
 }
 
 /// A Python module implemented in Rust.
